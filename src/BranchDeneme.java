ba62aeb6d0c5203f6531a9268c45daa6850bb02e
Yavuz <yavuzgecikli@hotmail.com>
2021-05-26 09:53:11 -0400

    feature modify ilk

--- src/BranchDeneme.java
+++ src/BranchDeneme.java
@@ -3,8 +3,8 @@ public class BranchDeneme {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("Feature branch 1. satir");
-		System.out.println("Feature branch 2. satir");
+		System.out.println("Feature branch 1 . satir");
+		System.out.println("Feature branch 2 . satir");
 		
 	}
 
